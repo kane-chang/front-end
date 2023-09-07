cf3fa6e007dabc6cabcb244731fe75dfc3f0bafb
Kane Chang <kane.chang.fl@gmail.com>
2023-09-07 22:24:34 +0100

    added functions to JavaScript file for prework-guide

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,20 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
 
-for (var x = 0; x < topics.length; x++) {
-    var topic = topics[x]
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+function selectTopic(number) {
+    var topic = topics[number]
     if (topic === 'HTML') {
         console.log("Let's study HTML!");
     } else if (topic === 'CSS') {
@@ -13,6 +26,10 @@ for (var x = 0; x < topics.length; x++) {
     } else {
         console.log('Please try again!');
     }
-
 }
 
+
+console.log('These are the topics we learned through Prework:')
+listTopics()
+console.log("Here's a random topic to study today:")
+selectTopic(getRandomInt(0, 4))
